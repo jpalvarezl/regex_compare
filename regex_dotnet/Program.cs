436f5c6f86f2aec85100f9e244227fc7838763bf
Jose Alvarez <jp.alvarezl@gmail.com>
2023-09-05 19:15:56 +0200

    Deserialization for dotnet done

--- regex_dotnet/Program.cs
+++ regex_dotnet/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+// Read the input.json file from the current directory
+string json = File.ReadAllText("../commons/input.json");
+
+// Deserialize the JSON array into a list of TextItem objects
+List<Entry> entries = JsonSerializer.Deserialize<List<Entry>>(json)!;
+
+// Print each item to the standard output
+foreach (var entry in entries)
+{
+    Console.WriteLine(entry.Text);
+}
+
+public class Entry
+{
+    [JsonPropertyName("text")]
+    public required string Text { get; set; }
+}
+
